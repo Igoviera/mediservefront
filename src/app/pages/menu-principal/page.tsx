5e25e2f5b48675e1a8c083d1ddbeebcda25a1e3a
Gabriel Evangelista <evangelistagabriel798@gmail.com>
2025-06-13 15:44:30 -0300

    refactor: melhoria no menu

--- src/app/pages/menu-principal/page.tsx
+++ src/app/pages/menu-principal/page.tsx
@@ -1,49 +1,18 @@
 import CardMenu from "@/components/Menu/MenuCard";
+import {opcoesMenu} from '@/lib/opcoesMenu';
 
 export default function MenuPrincipal() {
   return (
     <main className="bg-white border border-[#C8C8C8] rounded-md p-6 sm:mx-10 my-10">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        <CardMenu
-          href="/pages/agendar-consulta"
-          icon='/assets/icons/icon-agendamento.png'
-          label="Agendar Consultas"
-        />
-        <CardMenu 
-          href="" 
-          icon='/assets/icons/icon-documento.png' 
-          label="Consultas Agendadas" 
-        />
-        <CardMenu
-          href="/pages/cadastro-medico"
-          icon='/assets/icons/icon-usuario.png'
-          label="Cadastrar Médico"
-        />
-        <CardMenu
-          href="/pages/medicos-cadastrados"
-          icon='/assets/icons/icon-grupo.png'
-          label="Médicos Cadastrados"
-        />
-        <CardMenu 
-          href="" 
-          icon='/assets/icons/icon-usuario.png' 
-          label="Cadastrar Paciente" 
-        />
-        <CardMenu 
-          href="" 
-          icon='/assets/icons/icon-grupo.png' 
-          label="Pacientes Cadastrados" 
-        />
-        <CardMenu
-          href="/pages/cadastrar-usuarios"
-          icon='/assets/icons/icon-usuario.png'
-          label="Cadastrar Usuários"
-        />
-        <CardMenu
-          href="/pages/usuarios-cadastrados"
-          icon='/assets/icons/icon-grupo.png'
-          label="Usuários Cadastrados"
-        />
+        {opcoesMenu.map((opcao) => (
+          <CardMenu
+            key={opcao.label}
+            href={opcao.href}
+            icon={opcao.icon}
+            label={opcao.label}
+          />
+        ))}
       </div>
     </main>
   );
---
+++ src/lib/opcoesMenu.ts
@@ -0,0 +1,42 @@
+export const opcoesMenu = [
+  {
+    label: "Agendar Consultas",
+    icon: "/assets/icons/icon-agendamento.png",
+    href: "/pages/agendar-consulta",
+  },
+  {
+    label: "Consultas Agendadas",
+    icon: "/assets/icons/icon-documento.png",
+    href: "",
+  },
+  {
+    label: "Cadastrar Médico",
+    icon: "/assets/icons/icon-usuario.png",
+    href: "/pages/cadastro-medico",
+  },
+  {
+    label: "Médicos Cadastrados",
+    icon: "/assets/icons/icon-grupo.png",
+    href: "/pages/medicos-cadastrados",
+  },
+  {
+    label: "Cadastrar Paciente",
+    icon: "/assets/icons/icon-usuario.png",
+    href: "",
+  },
+  {
+    label: "Pacientes Cadastrados",
+    icon: "/assets/icons/icon-grupo.png",
+    href: "/pages/pacientes",
+  },
+  {
+    label: "Cadastrar Usuários",
+    icon: "/assets/icons/icon-usuario.png",
+    href: "/pages/cadastrar-usuarios",
+  },
+  {
+    label: "Usuários Cadastrados",
+    icon: "/assets/icons/icon-grupo.png",
+    href: "/pages/usuarios-cadastrados",
+  },
+];
